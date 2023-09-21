295a55861988f5c03a418d92447ddd8417b5f583
Quan <admin@MacBook-Pro-ca-Quan-Le.local>
2023-09-21 09:51:34 +0700

    init project

---
+++ app/src/main/java/com/quanle/countdown_timer_samples/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.quanle.countdown_timer_samples
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
